f247c4caba578f0128e254f5d63c52f29ed48a49
Richmond Safico <RichmondSafico42@gmail.com>
2026-06-10 20:33:03 +0800

    fix: celebrant section visibility on mobile + disort.svg now visible

--- src/App.jsx
+++ src/App.jsx
@@ -87,11 +87,11 @@ function App() {
       </div>
 
       <div className="scattered-container" aria-hidden="true">
-        <img src={disortDesign} alt="" className="scattered-design" style={{ top: '12%', left: '-80px', '--speed-x': '0.08', '--speed-y': '-0.12', '--rot-speed': '0.03', width: '220px' }} />
-        <img src={disortDesign} alt="" className="scattered-design" style={{ top: '35%', right: '-90px', '--speed-x': '-0.1', '--speed-y': '-0.15', '--rot-speed': '-0.04', width: '250px' }} />
-        <img src={disortDesign} alt="" className="scattered-design" style={{ top: '58%', left: '-70px', '--speed-x': '0.06', '--speed-y': '-0.1', '--rot-speed': '0.02', width: '200px' }} />
-        <img src={disortDesign} alt="" className="scattered-design" style={{ top: '76%', right: '-60px', '--speed-x': '-0.08', '--speed-y': '-0.12', '--rot-speed': '-0.03', width: '240px' }} />
-        <img src={disortDesign} alt="" className="scattered-design" style={{ top: '90%', left: '-50px', '--speed-x': '0.1', '--speed-y': '-0.18', '--rot-speed': '0.04', width: '210px' }} />
+        <img src={disortDesign} alt="" className="scattered-design" style={{ top: '5vh', left: '-40px', '--speed-x': '0.06', '--speed-y': '-0.08', '--rot-speed': '0.02', width: '180px' }} />
+        <img src={disortDesign} alt="" className="scattered-design" style={{ top: '30vh', right: '-50px', '--speed-x': '-0.07', '--speed-y': '-0.1', '--rot-speed': '-0.03', width: '200px' }} />
+        <img src={disortDesign} alt="" className="scattered-design" style={{ top: '55vh', left: '-30px', '--speed-x': '0.05', '--speed-y': '-0.07', '--rot-speed': '0.015', width: '160px' }} />
+        <img src={disortDesign} alt="" className="scattered-design" style={{ top: '75vh', right: '-40px', '--speed-x': '-0.06', '--speed-y': '-0.09', '--rot-speed': '-0.025', width: '190px' }} />
+        <img src={disortDesign} alt="" className="scattered-design" style={{ top: '90vh', left: '-35px', '--speed-x': '0.08', '--speed-y': '-0.12', '--rot-speed': '0.03', width: '170px' }} />
       </div>
 
       <div className="page">
